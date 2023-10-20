49dd35f99642cfe9d04cfd138bbdc47ee8acc54a
vaval <vavalol886@gmail.com>
2023-10-20 17:05:39 +0300

    project update

--- main.py
+++ main.py
@@ -3,34 +3,45 @@ from pyowm import OWM
 from pyowm.utils.config import get_default_config
 from tkinter import *
 import customtkinter
+from datetime import date, timedelta
 
 owm = OWM('69be55df671a26b648a0b198c0739f9e')
 config_dict = get_default_config()
-config_dict['language'] = 'ru'
+config_dict['language'] = 'uk'
 mgr = owm.weather_manager()
 
 
 def gettext():
     try:
-        observation = mgr.weather_at_place(textbox.get('1.0', END))
+        place =textbox.get('1.0', END).strip()
+        observation = mgr.weather_at_place(place)
         w = observation.weather
+        print(w)
         temp = w.temperature('celsius')["temp"]
-        print(temp)
+        status = w.detailed_status
+        detail_status = f'Температура {str(temp)} {status}'
+        label1.configure(text=detail_status)
     except pyowm.commons.exceptions.NotFoundError:
-        print("Не поняла")
+        label1.configure(text="Місто не було знайдено ")
+    except pyowm.commons.exceptions.APIRequestError:
+        label1.configure(text="Місто не було знайдено ")
 
 
 app = customtkinter.CTk()
 app.title("weather")
-app.geometry("400x150")
+app.geometry("240x200")
 
 
-textbox = customtkinter.CTkTextbox(app)
-label = customtkinter.CTkLabel(app, text="Введите город")
-button = customtkinter.CTkButton(app, text="Принять", command=gettext)
+textbox = customtkinter.CTkTextbox(app, width=200, height=20)
+label = customtkinter.CTkLabel(app, text="Введіть місто")
+label1 = customtkinter.CTkLabel(app, text="", fg_color="transparent")
+button = customtkinter.CTkButton(app, text="Пошук", command=gettext)
 
-label.grid(padx=10, pady=10)
-textbox.grid(padx=10, pady=10)
+label.grid(padx=5, pady=5)
+label1.grid(padx=0, pady=0)
+textbox.grid(padx=5, pady=5)
 button.grid(padx=50, pady=10)
-
 app.mainloop()
+
+today = date.today()
+next_week = today + timedelta(weeks=1)
